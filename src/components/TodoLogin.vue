cf39fdfc83234cb75b6745561e4a28a807142f67
Min Gyun Baek <scn06032@seoultech.ac.kr>
2022-05-25 14:47:09 +0900

    edit color

--- src/components/TodoLogin.vue
+++ src/components/TodoLogin.vue
@@ -4,18 +4,19 @@
         ><h2>Welcome</h2>
          <br /><br />
           <v-spacer></v-spacer>
-            <v-text-field v-model="email" outlined color='green' label="email"></v-text-field>
+            <v-text-field v-model="email" outlined color='#4caf50' label="email"></v-text-field>
             <v-text-field
               v-model="password"
-              outlined color ="green"
+              outlined color ="#4caf50"
               label="Password"
               hint="At least 6 characters"
               counter
             ></v-text-field>
         </v-form>
         <v-card-actions>
-    <v-btn outlined @click="addUser"> Signup </v-btn>
-    <v-btn outlined @click="login" color= 'green' width='80' > Login </v-btn>
+    <v-btn outlined @click="addUser" color ='#4caf50'> Signup </v-btn>
+    
+    <v-btn outlined @click="login" color= '#4caf50' > Login </v-btn>
     <v-btn @click="testStone">돌을 던지자</v-btn>
         </v-card-actions>
       
