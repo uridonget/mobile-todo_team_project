43189b39d1eba4d129a7b640d541393caf847232
Hae-Chan Jeong <ghflxn4@seoultech.ac.kr>
2022-05-27 01:39:13 +0900

    diaryAutoUpdate
    일기내용 불러와짐

--- src/components/TodoDiary.vue
+++ src/components/TodoDiary.vue
@@ -4,7 +4,6 @@
       뒤로가기
     </v-btn>
 
-
     <v-textarea
       v-model="diary"
       placeholder=""
@@ -29,6 +28,7 @@ import { getDatabase, ref, set, child, get } from "firebase/database";
 export default {
   data() {
     return {
+      diary: null,
       
     };
   },
@@ -36,6 +36,7 @@ export default {
   methods: {
     goback(){
       console.log("뒤로가기")
+      localStorage.removeItem("focusedDate");
       this.$router.replace("caldiary");
     },
 
@@ -47,13 +48,19 @@ export default {
       });
       
     },
-
-
-
   },
-
-  components: {
-    
-  },
+  created() {
+    const userinfo = JSON.parse(localStorage.getItem("userInfo"));
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/` + userinfo.uid + '/Diary/' + localStorage.getItem("focusedDate")))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          // console.log(data.diary)
+          this.diary = data.diary
+        }
+      })
+    },
 };
+
 </script>
